011cd3ee2c063eec155c0656abe4e0f746400dfc
sl41 <sl41@projectvalet.io>
2022-10-30 13:13:18 +0100

    Revert "update"
    This reverts commit ab5b1a875925b3c605a2b2741f8ae7fd4d52c655.

--- Assets/CameraNetworkMove.cs
+++ Assets/CameraNetworkMove.cs
@@ -1,22 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraNetworkMove : MonoBehaviour
 {
+    [SerializeField] public GameObject gameManagerObject;
+
+
+    private GameManager gameManagerReference;
     // Start is called before the first frame update
-    // void Start()
-    // {
-    //     
-    // }
+    void Start()
+    {
+        gameManagerReference = gameManagerObject.GetComponent<GameManager>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.Instance)
+        if (gameManagerReference)
         {
-            if (GameManager.Instance.currentState != GameManager.GameState.Preparation)
+            if (gameManagerReference.gameHasStarted)
             {
                 Vector3 currentLocation = transform.position;
-                Vector3 newTargetLocation = new Vector3(currentLocation.x, GameManager.Instance.currentCameraPositionY, currentLocation.z);
+                Vector3 newTargetLocation = new Vector3(currentLocation.x, gameManagerReference.CurrentCameraPositionY, currentLocation.z);
                 transform.position = Vector3.MoveTowards(currentLocation, newTargetLocation,
                     10.0f);
             }
